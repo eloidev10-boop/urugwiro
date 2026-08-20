2cc62b0f73ac96239cbb63039024811d0f96a6ed
the developer <eloidev10@gmail.com>
2026-08-20 20:46:29 +0200

    Update events.js

--- js/events.js
+++ js/events.js
@@ -5,100 +5,25 @@ EVENTS DATA
 
 const events = [
 
-    {
+   {
         id: 1,
-
-        type: "upcoming",
-
-        title: "Youth Leadership Summit",
-
-        date: "20 September 2026",
-
-        time: "09:00 AM",
-
-        location: "Saint Joseph Kabgayi",
-
-        organizer: "PLP Executive Committee",
-
-        image: "images/events/event1.jpg",
-
-        description:
-        "A leadership summit focused on empowering young members to become servant leaders within PLP and the wider community.",
-
-        gallery: [],
-
-        registration: true
-    },
-
-    {
-        id: 2,
-
-        type: "upcoming",
-
-        title: "Peace Walk",
-
-        date: "12 October 2026",
-
-        time: "08:00 AM",
-
-        location: "Muhanga Town",
-
-        organizer: "PLP Youth Commission",
-
-        image: "images/events/event2.jpg",
-
-        description:
-        "Members will walk together to promote peace, unity and reconciliation through community engagement.",
-
-        gallery: [],
-
-        registration: true
-    },
-
-    {
-        id: 3,
-
-        type: "upcoming",
-
-        title: "Monthly Prayer Meeting",
-
-        date: "30 September 2026",
-
-        time: "06:00 PM",
-
-        location: "PLP Hall",
-
-        organizer: "Spiritual Commission",
-
-        image: "images/events/event3.jpg",
-
-        description:
-        "Monthly gathering for prayer, worship and sharing the Word of God.",
-
-        gallery: [],
-
-        registration: false
-    },
-
-    {
-        id: 4,
 
         type: "past",
 
-        title: "Community Service",
+        title: "Kwibuka Event",
 
-        date: "10 May 2026",
+        date: "07 April 2026",
 
-        time: "08:30 AM",
+        time: "11:30 AM",
 
-        location: "Kabgayi",
+        location: "GISOZI",
 
-        organizer: "PLP Volunteers",
+        organizer: "PLP Urugwiro",
 
         image: "images/events/event4.jpg",
 
         description:
-        "Members served vulnerable families through cleaning, food distribution and community support.",
+        "Members took time for remembering those who were killed in GENOCIDE OF TUSTI IN 1994.",
 
         gallery: [
             "images/events/gallery/service1.jpg",
@@ -109,15 +34,33 @@ const events = [
         registration: false
     },
 
-    {
-        id: 5,
+       {
+        id: 2,
 
         type: "past",
 
-        title: "PLP Family Retreat",
+        title: "PLP Skill up",
 
         date: "15 July 2026",
 
-        time: "09:00 AM",
+        time: "14:00 AM",
 
-        location: "Huye
+        location: "Muhanga",
+
+        organizer: "PLP Family",
+
+        image: "images/events/event5.jpg",
+
+        description:
+        "A special PLP moment where we are skilled more about PLP.",
+
+        gallery: [
+            "images/events/gallery/retreat1.jpg",
+            "images/events/gallery/retreat2.jpg",
+            "images/events/gallery/retreat3.jpg"
+        ],
+
+        registration: false
+    }
+
+];
