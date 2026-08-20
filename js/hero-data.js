3f4de95df5381c01b505a4ce39cded1b32392db8
the developer <eloidev10@gmail.com>
2026-08-20 17:54:30 +0200

    Create hero-data.js

---
+++ js/hero-data.js
@@ -0,0 +1,53 @@
+const heroData = {
+
+memberName: "",
+
+welcome:
+"Welcome to the official Urugwiro Members Portal.",
+
+slides: [
+
+{
+image:"images/hero/hero1.jpg",
+title:"Peace",
+subtitle:"Living Together in Unity"
+},
+
+{
+image:"images/hero/hero2.jpg",
+title:"Leadership",
+subtitle:"Growing Responsible Leaders"
+},
+
+{
+image:"images/hero/hero3.jpg",
+title:"Service",
+subtitle:"Serving God and Community"
+}
+
+],
+
+statistics:[
+{
+number:50,
+suffix:"+",
+title:"Members"
+},
+{
+number:6,
+suffix:"+",
+title:"Projects"
+},
+{
+number:12,
+suffix:"+",
+title:"Events"
+},
+{
+number:1,
+suffix:"",
+title:"Years"
+}
+]
+
+}
